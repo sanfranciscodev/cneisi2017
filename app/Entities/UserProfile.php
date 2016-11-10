b5124be599a656ac2b20912c8aaa697213e18fd7
Ivan <colo9311@gmail.com>
2016-11-10 00:59:36 -0300

    Added attributtes and methods in UserProfile model.

--- app/Entities/UserProfile.php
+++ app/Entities/UserProfile.php
@@ -1,10 +1,61 @@
 <?php
-
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UserProfile extends Model
 {
-    //
+    private $type;
+    private $studentNumber;
+    private $university;
+
+    const UTN_STUDENT = 'utn_student';
+    const UTN_GRADUATED = 'utn_graduated';
+    const GENERAL_PUBLIC = 'general_public';
+
+    const ALL_TYPES = [self::UTN_STUDENT, self::UTN_GRADUATED, self::GENERAL_PUBLIC];
+
+    public function __construct(string $type)
+    {
+        if (!in_array(strtolower($type), self::ALL_TYPES)) {
+            throw new InvalidArgumentException("Invalid user type: {$type}");
+        } else {
+            $this->type = $type;
+        }
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function hasStudentNumber()
+    {
+        return !empty($this->studentNumber);
+    }
+
+    public function getStudentNumber()
+    {
+        return $this->studentNumber;
+    }
+
+    public function setStudentNumber(int $studentNumber)
+    {
+        $this->studentNumber = $studentNumber;
+    }
+
+    public function hasUniversity()
+    {
+        return !empty($this->university);
+    }
+
+    public function getUniversity()
+    {
+        return $this->university;
+    }
+
+    public function setUniversity(string $university)
+    {
+        $this->university = $university;
+    }
 }
